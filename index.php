300058b702f567d5d5dd07b7bb5acae0df7cbf76
Bernardo <bernardoags@gmail.com>
2019-10-13 08:37:17 -0300

    The ojs migrator can now be ran by eecuting the command bin/run

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
-require_once(dirname(__FILE__) . '/includes/bootstrap.php');
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    require_once(dirname(__FILE__) . '\\includes\\bootstrap.php');
+} else {
+    require_once(dirname(__FILE__) . '/includes/bootstrap.php');
+}
--- lib/classes/core/Application.php
+++ lib/classes/core/Application.php
@@ -6,6 +6,7 @@ namespace BeAmado\OjsMigrator;
 use \BeAmado\OjsMigrator\Util\FileSystemManager;
 use \BeAmado\OjsMigrator\Util\MemoryManager;
 use \BeAmado\OjsMigrator\Util\ArchiveManager;
+use \BeAmado\OjsMigrator\Util\IoManager;
 
 ///////////// Handlers ////////////////////////
 use \BeAmado\OjsMigrator\Util\ConfigHandler;
@@ -19,6 +20,7 @@ class Application
         Registry::set('FileSystemManager', new FileSystemManager());
         Registry::set('MemoryManager', new MemoryManager());
         Registry::set('ArchiveManager', new ArchiveManager());
+        Registry::set('IoManager', new IoManager());
     }
 
     protected function loadHandlers()
@@ -38,5 +40,13 @@ class Application
     {
         $this->loadManagers();
         $this->loadHandlers();
+    }
+
+    public function run()
+    {
+        $this->preload();
+        Registry::get('IoManager')->writeToStdout(
+            '############### OJS journal migration #################' . PHP_EOL
+        );
     }
 }
